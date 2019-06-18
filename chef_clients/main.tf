14c937935fe1fb4453e9ae8804d66f425c1a7043
Gavin Reynolds <gavin@chef.io>
2019-06-18 14:58:14 +0100

    Install python3-distutils on clients
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- chef_clients/main.tf
+++ chef_clients/main.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "chef_clients" {
   provisioner "remote-exec" {
     inline = [
       "set -Eeu",
-      "sudo apt update && sudo apt install -y ntp python3",
+      "sudo apt update && sudo apt install -y ntp python3 python3-distutils",
       "export LC_ALL='en_US.UTF-8' && export LC_CTYPE='en_US.UTF-8' && sudo dpkg-reconfigure  --frontend=noninteractive locales",
       "wget https://bootstrap.pypa.io/get-pip.py && sudo python3 get-pip.py -q",
       "sudo pip -q install awscli --upgrade",
