66a9c4b1a9b048a6879fe71d8ebd875596e75ff1
Gavin Reynolds <gavin@chef.io>
2020-10-01 19:03:42 +0100

    Fix up acm validation
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- chef_alb/main.tf
+++ chef_alb/main.tf
@@ -51,30 +51,28 @@ resource "aws_acm_certificate" "alb" {
   )
 }
 
-resource "aws_route53_record" "chef_cert_validation" {
-  name = aws_acm_certificate.alb.domain_validation_options[0].resource_record_name
-  type = aws_acm_certificate.alb.domain_validation_options[0].resource_record_type
-  zone_id = var.zone_id
-  records = [aws_acm_certificate.alb.domain_validation_options[0].resource_record_value]
-  ttl = 60
-}
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.alb.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-resource "aws_route53_record" "automate_cert_validation" {
-  name = aws_acm_certificate.alb.domain_validation_options[1].resource_record_name
-  type = aws_acm_certificate.alb.domain_validation_options[1].resource_record_type
-  zone_id = var.zone_id
-  records = [aws_acm_certificate.alb.domain_validation_options[1].resource_record_value]
-  ttl = 60
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.zone_id
 }
 
 resource "aws_acm_certificate_validation" "alb_cert" {
-  certificate_arn = aws_acm_certificate.alb.arn
-
-  validation_record_fqdns = [
-    aws_route53_record.chef_cert_validation.fqdn,
-    aws_route53_record.automate_cert_validation.fqdn,
-  ]
+  certificate_arn         = aws_acm_certificate.alb.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
 }
+
 
 resource "aws_route53_record" "chef_alb" {
   zone_id = var.zone_id
