ecdec0333b794cd5bedac2ca18d8bdef72333a2b
Gavin Reynolds <gavin@chef.io>
2020-10-01 19:05:33 +0100

    Fix up host_header
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- chef_alb/main.tf
+++ chef_alb/main.tf
@@ -38,7 +38,7 @@ locals {
 # }
 
 resource "aws_acm_certificate" "alb" {
-  domain_name = local.chef_alb_fqdn
+  domain_name               = local.chef_alb_fqdn
   subject_alternative_names = [local.automate_alb_fqdn]
 
   validation_method = "DNS"
@@ -76,23 +76,23 @@ resource "aws_acm_certificate_validation" "alb_cert" {
 
 resource "aws_route53_record" "chef_alb" {
   zone_id = var.zone_id
-  name = local.chef_alb_fqdn
-  type = "CNAME"
-  ttl = var.r53_ttl
+  name    = local.chef_alb_fqdn
+  type    = "CNAME"
+  ttl     = var.r53_ttl
   records = [module.alb.this_lb_dns_name]
 }
 
 resource "aws_route53_record" "automate_alb" {
   zone_id = var.zone_id
-  name = local.automate_alb_fqdn
-  type = "CNAME"
-  ttl = var.r53_ttl
+  name    = local.automate_alb_fqdn
+  type    = "CNAME"
+  ttl     = var.r53_ttl
   records = [module.alb.this_lb_dns_name]
 }
 
 module "alb" {
-  source = "terraform-aws-modules/alb/aws"
-  name = "${replace(local.chef_alb_fqdn, ".", "-")}-alb"
+  source          = "terraform-aws-modules/alb/aws"
+  name            = "${replace(local.chef_alb_fqdn, ".", "-")}-alb"
   security_groups = [var.https_security_group_id]
   # access_logs = {
   #   bucket = aws_s3_bucket.alb_logs.bucket
@@ -111,50 +111,51 @@ module "alb" {
   https_listeners = [
     {
       "certificate_arn" = aws_acm_certificate.alb.arn
-      "port" = 443
-      "ssl_policy" = "ELBSecurityPolicy-TLS-1-2-2017-01"
+      "port"            = 443
+      "ssl_policy"      = "ELBSecurityPolicy-TLS-1-2-2017-01"
     },
   ]
 
   target_groups = [
     {
-      "name" = "automate-https"
+      "name"             = "automate-https"
       "backend_protocol" = "HTTPS"
-      "backend_port" = "443"
+      "backend_port"     = "443"
     },
     {
-      "name" = "chef-https"
+      "name"             = "chef-https"
       "backend_protocol" = "HTTPS"
-      "backend_port" = "443"
+      "backend_port"     = "443"
     },
   ]
 }
 
 resource "aws_lb_target_group_attachment" "automate-https" {
-  count = "1"
+  count            = "1"
   target_group_arn = element(module.alb.target_group_arns, 0)
-  target_id = element(var.automate_target_ids, count.index)
-  port = 443
+  target_id        = element(var.automate_target_ids, count.index)
+  port             = 443
 }
 
 resource "aws_lb_target_group_attachment" "chef-https" {
-  count = length(var.chef_target_ids)
+  count            = length(var.chef_target_ids)
   target_group_arn = element(module.alb.target_group_arns, 1)
-  target_id = element(var.chef_target_ids, count.index)
-  port = 443
+  target_id        = element(var.chef_target_ids, count.index)
+  port             = 443
 }
 
 resource "aws_lb_listener_rule" "forward_to_automate" {
   listener_arn = element(module.alb.https_listener_arns, 0)
 
   action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = element(module.alb.target_group_arns, 0)
   }
 
   condition {
-    field = "host-header"
-    values = [local.automate_alb_fqdn]
+    host_header {
+      values = [local.automate_alb_fqdn]
+    }
   }
 }
 
@@ -162,12 +163,13 @@ resource "aws_lb_listener_rule" "forward_to_chef" {
   listener_arn = element(module.alb.https_listener_arns, 0)
 
   action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = element(module.alb.target_group_arns, 1)
   }
 
   condition {
-    field = "host-header"
-    values = [local.chef_alb_fqdn]
+    host_header {
+      values = [local.chef_alb_fqdn]
+    }
   }
 }
