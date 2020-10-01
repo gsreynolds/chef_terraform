012bcaac533e1fe965ab175913f8be108768b634
Gavin Reynolds <gavin@chef.io>
2020-10-01 19:49:15 +0100

    Update iam token creation
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- chef_automate2/main.tf
+++ chef_automate2/main.tf
@@ -50,7 +50,7 @@ resource "aws_instance" "automate_server" {
       "sudo chef-automate deploy --channel current --upgrade-strategy none --accept-terms-and-mlsa config.toml",
       "sudo chef-automate license apply \"${var.automate_license}\"",
       "sudo chown ${var.ami_user}:${var.ami_user} automate-credentials.toml",
-      "echo api-token = \"$(sudo chef-automate admin-token)\" >> automate-credentials.toml",
+      "echo api-token = \"$(sudo chef-automate iam token create admin --admin)\" >> automate-credentials.toml",
     ]
   }
 }
