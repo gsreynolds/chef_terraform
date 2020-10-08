f1fd083d1f25224930313b4eda60f1a449018410
Gavin Reynolds <gavin@chef.io>
2020-10-08 11:29:55 +0100

    Add ingest notes
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- chef_automate2/iam/iam-example-commands.sh
+++ chef_automate2/iam/iam-example-commands.sh
@@ -2,7 +2,9 @@ curl -s -H "api-token: $TOKEN" https://localhost/apis/iam/v2/policies -k | jq
 curl -s -H "api-token: $TOKEN" https://localhost/apis/iam/v2/policies/administrator-access -k | jq
 
 # Add members
-curl -s -H "api-token: $TOKEN" -H "Content-Type: application/json" https://localhost/apis/iam/v2/policies/administrator-access/members:add -k -d @administrator-access-members-add.json | jq
+curl -s -H "api-token: $TOKEN" -H "Content-Type: application/json" https://localhost/apis/iam/v2/policies/administrator-access/members:add -k -d @administrator-access-m
+# Assign "ingest" token to ingest policy
+# sudo chef-automate iam token create ingest
 curl -s -H "api-token: $TOKEN" -H "Content-Type: application/json" https://localhost/apis/iam/v2/policies/administrator-access/members:add -k -d '{"members":["token:ingest"]}' | jq
 # Replace members
 curl -s -H "api-token: $TOKEN" -H "Content-Type: application/json" https://localhost/apis/iam/v2/policies/administrator-access/members -k -d @administrator-access-members-add.json -X PUT | jq
